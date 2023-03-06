411a6f9436c318682d43fde639d769ab9317ec27
ivan-pesenti <ivan.pesenti.dev@gmail.com>
2023-03-06 14:50:57 +0100

    ID - HTTP - Red

--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -15,9 +15,26 @@ func TestUpdateTodo_IdNotInteger(t *testing.T) {
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
 
-	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "abcd"})
 
 	UpdateTodo(c)
 
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestUpdateTodo_IdNotInteger_Body(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	expectedBody := `"{ \"code\": \"validation err\", \"message\": \"strconv.Atoi: parsing \\\"abc\\\": invalid syntax\" }"`
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "abcd"})
+
+	UpdateTodo(c)
+
+	if w.Body.String() != expectedBody {
+		t.Errorf("expected %q got %q", expectedBody, w.Body.String())
+	}
 }
