2dddc26e8d4f30dbc0f31c7ff2fadbf48e918b6c
ivan-pesenti <ivan.pesenti.dev@gmail.com>
2023-03-06 17:02:07 +0100

    DB - HTTP - Refactor2

--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -14,19 +14,6 @@ import (
 func TestUpdateTodo_IdNotInteger(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-
-	c.Params = append(c.Params, gin.Param{Key: "id", Value: "abcd"})
-
-	UpdateTodo(c)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-}
-
-func TestUpdateTodo_IdNotInteger_Body(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-
 	var todoErr TodoErr
 
 	w := httptest.NewRecorder()
@@ -40,5 +27,6 @@ func TestUpdateTodo_IdNotInteger_Body(t *testing.T) {
 		t.Fatalf("err not expected while unmarshaling: %v", err)
 	}
 
+	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Equal(t, models.ValidationErr, todoErr.Code)
 }
