5a02553748f0721dfffa908638671ab9c1e5d78b
ivan-pesenti <ivan.pesenti.dev@gmail.com>
2023-03-07 10:25:50 +0100

    FOU - HTTP - Red

--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -14,20 +14,16 @@ import (
 
 func TestUpdateTodo_IdNotInteger(t *testing.T) {
 	gin.SetMode(gin.TestMode)
-
-	var todoErr TodoErr
-
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
-
 	c.Params = append(c.Params, gin.Param{Key: "id", Value: "abcd"})
 
 	UpdateTodo(c)
 
+	var todoErr TodoErr
 	if err := json.Unmarshal(w.Body.Bytes(), &todoErr); err != nil {
 		t.Fatalf("err not expected while unmarshaling: %v", err)
 	}
-
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Equal(t, models.IdNotIntegerErr, todoErr.Code)
 }
@@ -36,9 +32,7 @@ func TestUpdateTodo_ValidationErr(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
-
 	req := httptest.NewRequest("PUT", "/todos", strings.NewReader(`{ "description": "", "is_completed": true, "due_date": "2023-05-04" }`))
-
 	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
 	c.Request = req
 
@@ -48,7 +42,20 @@ func TestUpdateTodo_ValidationErr(t *testing.T) {
 	if err := json.Unmarshal(w.Body.Bytes(), &todoErr); err != nil {
 		t.Fatalf("err not expected while unmarshaling: %v", err)
 	}
-
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Equal(t, models.ValidationErr, todoErr.Code)
+}
+
+func TestUpdateTodo_NotFound(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	req := httptest.NewRequest("PUT", "/todos", strings.NewReader(`{ "description": "lorem ipsum", "is_completed": true, "due_date": "2023-05-04" }`))
+	req.Header.Set("Content-Type", "application/json")
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+	c.Request = req
+
+	UpdateTodo(c)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
 }
