e6a21e6a8ae1f9a9e2687695d7a7597f058701a4
ivan-pesenti <ivan.pesenti.dev@gmail.com>
2023-03-15 15:23:04 +0100

    HAP - HTTP - Red

--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -109,3 +109,37 @@ func TestUpdateTodo_DbError(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestUpdateTodo_HappyPath(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	w := httptest.NewRecorder()
+	c := gin.CreateTestContextOnly(w, gin.Default())
+
+	db, mock, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	defer db.Close()
+	conn, _ := db.Conn(c)
+	defer conn.Close()
+	gormDb, _ := gorm.Open(postgres.New(postgres.Config{
+		Conn: conn,
+	}), &gorm.Config{})
+
+	rows := sqlmock.NewRows([]string{"id", "description", "is_completed", "due_date"}).AddRow(1, "lorem ipsum", false, "2023-05-01")
+	mock.ExpectQuery(`SELECT * FROM "todos" WHERE "todos"."id" = $1 ORDER BY "todos"."id" LIMIT 1`).WithArgs(1).WillReturnRows(rows)
+	mock.ExpectBegin()
+	mock.ExpectExec(`UPDATE "todos" SET "id"=$1,"description"=$2,"is_completed"=$3,"due_date"=$4 WHERE "todos"."id" = $5 AND "id" = $6`).WithArgs(1, "lorem ipsum", true, "2023-05-04", 1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectCommit()
+
+	c.Request = &http.Request{
+		Header: make(http.Header),
+		URL:    &url.URL{},
+	}
+	c.Request.Method = http.MethodPut
+	c.Request.Header.Set("Content-Type", "application/json")
+	c.Request.Body = io.NopCloser(strings.NewReader(`{ "description": "lorem ipsum", "is_completed": true, "due_date": "2023-05-04" }`))
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+	c.Set(DBKey, gormDb)
+
+	UpdateTodo(c)
+
+	assert.Equal(t, 200, w.Code)
+}
