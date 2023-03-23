3293c2ff6a267a609198faa37825e6153c12a589
ivan-pesenti <ivan.pesenti.dev@gmail.com>
2023-03-23 10:51:57 +0100

    add example on how to emulate intensive workload on Postgres

--- repo/todo.go
+++ repo/todo.go
@@ -10,6 +10,9 @@ import (
 )
 
 func UpdateTodo(db *gorm.DB, todoToSave models.Todo) error {
+	// this is to emulate an intensive workload on Postgres
+	// var test string
+	// db.Debug().Raw("select pg_sleep(5) as test;").Scan(&test)
 	if err := db.First(&models.Todo{}, todoToSave.ID).Updates(todoToSave).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return models.TodoErr{StatusCode: http.StatusNotFound, Code: models.TodoNotFoundErr, Message: err.Error()}
